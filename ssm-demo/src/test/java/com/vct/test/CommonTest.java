61e2adc20f481081a7e5788b84b92343e342da14
Liuzz <zhang0909990@qq.com>
2020-05-21 18:26:45 +0800

    1.集成rocketmq 2.新增单元测试

--- ssm-demo/src/test/java/com/vct/test/CommonTest.java
+++ ssm-demo/src/test/java/com/vct/test/CommonTest.java
@@ -1,5 +1,9 @@
 package com.vct.test;
 
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -10,9 +14,27 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class CommonTest {
 
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:dubbo-config.xml","classpath:spring/spring-service.xml");
-        context.start();
+    public static void main(String[] args) throws Exception{
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("MyProducerGroup");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("liuzz.vm.com:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TonnyYa" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
     }
 
 }
